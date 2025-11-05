a1b7b239aab8afe2a474b40cd04d70038eb34607
Fluder-Paradyne <121793617+Fluder-Paradyne@users.noreply.github.com>
2025-11-05 23:23:53 +0530

    refactor(timeout): remove redundant comment about soft timeout as default behavior

--- option.go
+++ option.go
@@ -24,8 +24,6 @@ func WithResponse(h gin.HandlerFunc) Option {
 	}
 }
 
-// Soft timeout is the default behavior; no extra options are required.
-
 func defaultResponse(c *gin.Context) {
 	c.String(http.StatusRequestTimeout, http.StatusText(http.StatusRequestTimeout))
 }
